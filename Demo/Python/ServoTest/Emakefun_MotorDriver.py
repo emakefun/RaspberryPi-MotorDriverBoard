ca9ed7580e3d40da51bf26100e6de21875cb1b3e
tom <tom@emakefun.com>
2021-03-29 09:45:01 +0800

    修改python3 运行出错问题，添加MD注意事项常见问题

--- Demo/Python/ServoTest/Emakefun_MotorDriver.py
+++ Demo/Python/ServoTest/Emakefun_MotorDriver.py
@@ -79,14 +79,14 @@ class PWM :
 
   def setPWM(self, channel, on, off):
     "Sets a single PWM channel"
-    self.i2c.write8(self.__LED0_ON_L+4*channel, on & 0xFF)
-    self.i2c.write8(self.__LED0_ON_H+4*channel, on >> 8)
-    self.i2c.write8(self.__LED0_OFF_L+4*channel, off & 0xFF)
-    self.i2c.write8(self.__LED0_OFF_H+4*channel, off >> 8)
+    self.i2c.write8(self.__LED0_ON_L+4*channel, int(on) & 0xFF)
+    self.i2c.write8(self.__LED0_ON_H+4*channel, int(on) >> 8)
+    self.i2c.write8(self.__LED0_OFF_L+4*channel, int(off) & 0xFF)
+    self.i2c.write8(self.__LED0_OFF_H+4*channel, int(off) >> 8)
 
   def setAllPWM(self, on, off):
     "Sets a all PWM channels"
-    self.i2c.write8(self.__ALL_LED_ON_L, on & 0xFF)
-    self.i2c.write8(self.__ALL_LED_ON_H, on >> 8)
-    self.i2c.write8(self.__ALL_LED_OFF_L, off & 0xFF)
-    self.i2c.write8(self.__ALL_LED_OFF_H, off >> 8)
+    self.i2c.write8(self.__ALL_LED_ON_L, int(on) & 0xFF)
+    self.i2c.write8(self.__ALL_LED_ON_H, int(on) >> 8)
+    self.i2c.write8(self.__ALL_LED_OFF_L, int(off) & 0xFF)
+    self.i2c.write8(self.__ALL_LED_OFF_H, int(off) >> 8)
